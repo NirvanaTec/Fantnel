ca330e1f43e8f564ae00c9d7eeee21a8aef4bddb
NirvanaTec <fengheng1111@126.com>
2025-12-13 16:27:35 +0800

    涅槃科技

--- Fantnel/Servlet/HomeController.cs
+++ Fantnel/Servlet/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using NirvanaPublic.Entities;
 
@@ -8,9 +8,18 @@ namespace Fantnel.Servlet;
 [Route("[controller]")]
 public class HomeController : ControllerBase
 {
+    
     [HttpGet("/api/home")]
-    public IActionResult Index()
+    public IActionResult HomeInfo()
     {
         return Content(JsonSerializer.Serialize(InfoManager.FantnelInfo), "application/json");
     }
+    
+    public static string GetIndexHtml()
+    {
+        // 获取运行目录路径
+        var resourcesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources", "static", "index.html");
+        return System.IO.File.ReadAllText(resourcesPath);
+    }
+    
 }
