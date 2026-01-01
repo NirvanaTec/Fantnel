6e8006e444e3f3fd076ce9617c08c6dbf148707e
风横 <3547694806@qq.com>
2026-01-01 05:55:15 +0000

    涅槃科技
    Signed-off-by: 风横 <3547694806@qq.com>

---
+++ Codexus.Game.Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyCompany("Codexus.Game.Launcher")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0+e1d1d59a225ed44a4522df523f767a2b326e06c5")]
+[assembly: AssemblyProduct("Codexus.Game.Launcher")]
+[assembly: AssemblyTitle("Codexus.Game.Launcher")]
+[assembly: AssemblyVersion("1.0.0.0")]
